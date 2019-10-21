4170f61d94a1628cff781eb33b47160eba70808a
atange-filmskolen <52198175+atange-filmskolen@users.noreply.github.com>
2019-10-21 15:10:44 +0200

    added textmesh pro and made debug canvas prefab with load buttons

---
+++ GraduationGame unity/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void loadNextScene()
+    {
+        if(getCurrentScene() + 1 < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(getCurrentScene() + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void loadPreviousScene()
+    {
+        if(getCurrentScene() - 1 > 0)
+        {
+            SceneManager.LoadScene(getCurrentScene() - 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings-1);
+        }
+    }
+
+    public void reLoadCurrentScene()
+    {
+        SceneManager.LoadScene(getCurrentScene());
+    }
+
+    private int getCurrentScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        return currentScene;
+    }
+}
