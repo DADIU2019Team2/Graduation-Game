b75523f55e72b2347459cd0651b352e93813efe4
atange-filmskolen <52198175+atange-filmskolen@users.noreply.github.com>
2019-10-21 15:29:36 +0200

    debug menu is toggleable

---
+++ GraduationGame unity/Assets/Scripts/ToggleGameObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGameObject : MonoBehaviour
+{
+    public void toggleGameObject()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
