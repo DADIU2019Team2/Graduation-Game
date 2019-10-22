7ee2c63619993b47b8616fce35cd8982591579c5
atange-filmskolen <52198175+atange-filmskolen@users.noreply.github.com>
2019-10-22 11:25:00 +0200

    now displayes fpscounter

--- GraduationGame unity/Assets/Scripts/FpsCounter.cs
+++ GraduationGame unity/Assets/Scripts/FpsCounter.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FpsCounter : MonoBehaviour
 {
+    public enum FPSType{avgFPS, fps, highestFPS, LowestFPS}
+    public FPSType fpsType;
+    public TextMeshProUGUI tmText;
     public static float fps;
+    public static float actualFPS;
     public static float avgFPS { get; set; }
     public static float highestFPS { get; set; }
     public static float lowestFPS { get; set; }
     public int frameRange;
     int[] fpsBuffer;
     int fpsBufferIndex;
+    Queue<int> oneSecfpsBuffer;
+    int framesInLastOneSec;
+    //int oneSecFPSIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +33,20 @@ public class FpsCounter : MonoBehaviour
         {
             InitializeBuffer();
         }
+        updateOneSecFPS();
         UpdateBuffer();
         CalculateFPS();
-        Debug.Log("Avg FPS : " + avgFPS + ", Highest FPS : " + highestFPS + ", Lowest FPS : " + +lowestFPS + "    Value are for last " + frameRange + " frames");
+        //Debug.Log("Avg FPS : " + avgFPS + ", Highest FPS : " + highestFPS + ", Lowest FPS : " + +lowestFPS + "    Value are for last " + frameRange + " frames");
+        if(tmText != null)
+        {
+            writeFPS();
+        }
+    }
+
+    private void updateOneSecFPS()
+    {
+        oneSecfpsBuffer.Enqueue(framesInLastOneSec);
+        StartCoroutine(popQueAfterSec());
     }
     void InitializeBuffer()
     {
@@ -36,6 +56,9 @@ public class FpsCounter : MonoBehaviour
         }
         fpsBuffer = new int[frameRange];
         fpsBufferIndex = 0;
+
+        oneSecfpsBuffer = new Queue<int>();
+        framesInLastOneSec = 0;
     }
 
     void UpdateBuffer()
@@ -74,16 +97,46 @@ public class FpsCounter : MonoBehaviour
         lowestFPS = lowest;
     }
 
-    public static float GetAverageFPS()
+    public float getFPSCount()
+    {
+        return oneSecfpsBuffer.Count;
+    }
+    public float GetAverageFPS()
     {
         return avgFPS;
     }
-    public static float GetHighestFPS()
+    public float GetHighestFPS()
     {
         return highestFPS;
     }
-    public static float GetLowestFPS()
+    public float GetLowestFPS()
     {
         return lowestFPS;
+    }
+
+    public void writeFPS()
+    {
+        switch (fpsType)
+        {
+        case FPSType.fps:
+            tmText.text = "FPS count: " + getFPSCount().ToString();
+            break;
+        case FPSType.avgFPS:
+            tmText.text = "avg FPS: " + GetAverageFPS().ToString();
+            break;
+        case FPSType.highestFPS:
+            tmText.text = "Highest FPS: " + GetHighestFPS().ToString();
+            break;
+        case FPSType.LowestFPS:
+            tmText.text = "Lowest FPS: " + GetLowestFPS().ToString();
+            break;
+        }
+    }
+
+    IEnumerator popQueAfterSec()
+    {
+        yield return new WaitForSeconds(1f);
+        if(oneSecfpsBuffer.Count > 0)
+            oneSecfpsBuffer.Dequeue();
     }
 }
