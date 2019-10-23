c77dbdf4cf41f61779bf6a85138410bae7445b22
LasseAafeldt <superlla@hotmail.com>
2019-10-23 13:02:29 +0200

    added scriptableobject monobehaviour inspector

---
+++ GraduationGame unity/Assets/Scripts/AnimatorControllerInspetor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+using UnityEditor;
+
+public class AnimatorControllerInspetor : MonoBehaviour
+{
+    //[SerializeField]public AnimatorControllerParameter parameter; cant see in inspector
+    //public RuntimeAnimatorController runtimeController;
+    public AnimatorController controller;
+    [SerializeField]public AnimatorControllerParameter[] controllerParams;
+
+    private void Start()
+    {        
+        controllerParams = controller.parameters;
+    }
+}
