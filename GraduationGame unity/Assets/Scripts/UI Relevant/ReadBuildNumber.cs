6d9e9c2544b4b06d80f6b75043b5ebec472594f0
atange-filmskolen <52198175+atange-filmskolen@users.noreply.github.com>
2019-10-23 09:46:02 +0200

    buildnumber should now be read and displayed properly

---
+++ GraduationGame unity/Assets/Scripts/UI Relevant/ReadBuildNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+using UnityEditor;
+
+public class ReadBuildNumber : MonoBehaviour
+{
+    void Start()
+    {
+        if (GetComponent<TextMeshProUGUI>() != null)
+        {
+            TextMeshProUGUI textElement = GetComponent<TextMeshProUGUI>();
+            textElement.text = readBuildNRFile();
+        }
+    }
+
+    string readBuildNRFile()
+    {
+        /*string buildNumFilePath = Application.dataPath + "/buildNumbers.txt";
+        string[] everyLine = File.ReadAllLines(buildNumFilePath);*/
+
+        TextAsset buildNRFile = Resources.Load("buildNumbers") as TextAsset;
+        if (buildNRFile == null) {return "Build #null"; }
+        string allLines = buildNRFile.text;
+        string[] everyLine = new string[3];
+        if (allLines.Count<Char>() > 0)
+        {
+            everyLine = allLines.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+        }
+
+        if (everyLine.Length > 0)
+        {
+            return everyLine[2] + " Build NR: " + everyLine[1];//the number should be in the second line always
+        }
+        return "There is no build number to be found";
+    }
+}
