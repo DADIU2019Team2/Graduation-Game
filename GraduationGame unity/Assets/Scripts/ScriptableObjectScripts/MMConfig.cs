3676441edc65cfe1c3f162718917fb8954cd235a
LasseAafeldt <superlla@hotmail.com>
2019-10-29 11:03:23 +0100

    fixed build problems cause of editor libraries

--- GraduationGame unity/Assets/Scripts/ScriptableObjectScripts/MMConfig.cs
+++ GraduationGame unity/Assets/Scripts/ScriptableObjectScripts/MMConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -34,3 +35,4 @@ public class MMConfig : ScriptableObject
     }
     
 }
+#endif
