d6f65ecbc3da2a2c5974448a4950d58945942503
Rutaceae <jamiemoeller@yahoo.dk>
2019-10-29 15:39:26 +0100

    Added scriptableObject for SwipeAngleThresholds and finalized InputManager. Added OnInputEvent too.

--- GraduationGame unity/Assets/Scripts/InputManager.cs
+++ GraduationGame unity/Assets/Scripts/InputManager.cs
@@ -1,17 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MiniGame2.Events;
 
 public class InputManager : MonoBehaviour
 {
+    public enum SwipeType
+    {
+        swipeForwardUp /*For forward jumps */,
+        swipeBackwards /*For changing direction and ledge-jumping backwards */,
+        swipeDown /*For sliding and dropping down ledges */,
+        none //only ever happening if a bug occured.
+    }
+
     private bool isDragging;
-
     Vector2 initPosition, endPosition;
-
     Vector2 swipeDirection;
     float swipeAngle;
+    private bool isFacingRight;
+    public SwipeAngleThresholds swipeAngleThresholds;
+    public VoidEvent OnSwipeEvent;
+    private SwipeType mostRecentSwipeType;
 
-    public CharacterMovement player;
+
+
+    private void Start()
+    {
+        //Debug.Assert(mirroredAngle(10) == 170, "10 mirrored = 170");
+        //Debug.Assert(mirroredAngle(190) == -10, "190 mirrored = -10");
+
+
+    }
+
+    float mirroredAngle(float angle)
+    {
+        return 540 - angle; //540 chosen to avoid negative numbers.
+    }
 
     void Update()
     {
@@ -26,15 +50,58 @@ public class InputManager : MonoBehaviour
 
                 case TouchPhase.Ended:
                     endPosition = touch.position;
-                    swipeDirection = (endPosition - initPosition);  
-                    
+                    swipeDirection = (endPosition - initPosition);
                     swipeAngle = Vector2.Angle(Vector2.right, swipeDirection);
-                    
+                    /*isFacingRight = CharacterController.GetIsFacingRight();*/ //Uncomment this when merged.
+                    isFacingRight = true;
+                    mostRecentSwipeType = SwipeTypeOfAngle(swipeAngle, isFacingRight);
+                    OnSwipeEvent.Raise();
                     //TODO: Implement player using swipe here.
 
                     break;
             }
         }
+    }
+
+    //Should take into account different input-types when they are implemented.
+    public SwipeType GetMostRecentInputType()
+    {
+        return mostRecentSwipeType; 
+    }
+    public SwipeType SwipeTypeOfAngle(float swipeAngle, bool isFacingRight)
+    {
+
+        if (isFacingRight)
+        {
+            if (swipeAngle > swipeAngleThresholds.swipeUpRightAngles0 && swipeAngle < swipeAngleThresholds.swipeUpRightAngles1)
+            {
+                return SwipeType.swipeForwardUp;
+            }
+            if (swipeAngle > swipeAngleThresholds.swipeBackwardsAngles0 && swipeAngle < swipeAngleThresholds.swipeBackwardsAngles1)
+            {
+                return SwipeType.swipeBackwards;
+            }
+            if (swipeAngle > swipeAngleThresholds.swipeDownAngles0 && swipeAngle < swipeAngleThresholds.swipeDownAngles1)
+            {
+                return SwipeType.swipeDown;
+            }
+        }
+        else if (!isFacingRight)
+        {
+            if (swipeAngle < mirroredAngle(swipeAngleThresholds.swipeUpRightAngles0) && swipeAngle > mirroredAngle(swipeAngleThresholds.swipeUpRightAngles1))
+            {
+                return SwipeType.swipeForwardUp;
+            }
+            if (swipeAngle < mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles0) && swipeAngle > mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles1))
+            {
+                return SwipeType.swipeBackwards;
+            }
+            if (swipeAngle < mirroredAngle(swipeAngleThresholds.swipeDownAngles0) && swipeAngle > mirroredAngle(swipeAngleThresholds.swipeDownAngles1))
+            {
+                return SwipeType.swipeDown;
+            }
+        }
+        return SwipeType.none; //Stuff bugged out or the inputsettings on SwipeAngleThreshholds
     }
 
 }
---
+++ GraduationGame unity/Assets/Scripts/ScriptableObjectScripts/SwipeAngleThresholds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SwipeAngleThresholds", menuName = "ScriptableObject/SwipeAngleThreshholds")]
+public class SwipeAngleThresholds : ScriptableObject
+{
+    [Range(-360, 360)]
+    public float swipeUpRightAngles0, swipeUpRightAngles1, 
+    swipeBackwardsAngles0, swipeBackwardsAngles1, 
+    swipeDownAngles0, swipeDownAngles1;
+}
