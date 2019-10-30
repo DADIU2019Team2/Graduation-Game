9b298d7c7d7ba9a5b70276454863feaa44d00bc8
fredrig <neumannze@hotmail.com>
2019-10-30 11:30:55 +0100

    Added footsteps script to zoe

--- GraduationGame unity/Assets/Scripts/Sound Scripts/Footstep_Velocity_Loop.cs
+++ GraduationGame unity/Assets/Scripts/Sound Scripts/Footstep_Velocity_Loop.cs
@@ -8,13 +8,13 @@ public class Footstep_Velocity_Loop : MonoBehaviour
     void Start()
     {
        // Plays the footstep loop soundfile on start - only heard when velocity is above 0
-       AkSoundEngine.PostEvent("Footstep_loop_placeholder", gameObject);
+       AkSoundEngine.PostEvent("Play_Footstep_loop_PH", gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         // When velocity is 0 the footstep loop is not playing - when it is above 0 it plays
-        AkSoundEngine.SetRTPCValue("Footstep_velocity", null);
+        AkSoundEngine.SetRTPCValue("Footstep_velocity", FindObjectOfType<CharacterMovement>().GetVelocity().magnitude);
     }
 }
