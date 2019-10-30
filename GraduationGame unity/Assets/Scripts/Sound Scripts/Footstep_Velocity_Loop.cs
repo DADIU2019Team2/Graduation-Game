271c330575c42193b2fe579b8e8e6b9301236379
fredrig <neumannze@hotmail.com>
2019-10-30 11:04:55 +0100

    Added script in unity + rtpc in wwise

---
+++ GraduationGame unity/Assets/Scripts/Sound Scripts/Footstep_Velocity_Loop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footstep_Velocity_Loop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Plays the footstep loop soundfile on start - only heard when velocity is above 0
+       AkSoundEngine.PostEvent("Footstep_loop_placeholder", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When velocity is 0 the footstep loop is not playing - when it is above 0 it plays
+        AkSoundEngine.SetRTPCValue("Footstep_velocity", null);
+    }
+}
