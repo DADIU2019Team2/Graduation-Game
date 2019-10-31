8ce3b7a83fb4fda58e2eea89b00846b5b94882af
Rutaceae <jamiemoeller@yahoo.dk>
2019-10-31 11:50:11 +0100

    Change to gamemanager.cs to work with ScriptableObj

--- GraduationGame unity/Assets/Scripts/GameManager.cs
+++ GraduationGame unity/Assets/Scripts/GameManager.cs
@@ -6,37 +6,37 @@ public class GameManager : MonoBehaviour
 {
 
     [SerializeField]
-    private static GameState gameState;
+    private static GameStateScriptableObject.GameState gameState;
 
     // Update is called once per frame
     void Update()
     {
         switch (gameState)
         {
-            case GameState.levelStart:
+            case GameStateScriptableObject.GameState.levelStart:
                 /*Fade from black. 
                 Nothing happens until player gives some sort of input to start the level. 
                 Transitions into gameplay-state. */
                 break;
             #region maingameplay
-            case GameState.mainGameplayLoop:
+            case GameStateScriptableObject.GameState.mainGameplayLoop:
                 //Main logic of the game goes on here. The player has control over Zoe. 
                 break;
             #endregion maingameplay
-            case GameState.levelLoss:
+            case GameStateScriptableObject.GameState.levelLoss:
                 /*Fade to black, return all objects in scene to their initial states. 
                 “Reload” scene and fade back into level-start state. 
                 Zoe and camerashould be returned to most recent checkpoint met, rather than at the initial position at start of the level */
                 break;
-            case GameState.cinematic:
+            case GameStateScriptableObject.GameState.cinematic:
                 /*(No player control at all until they end)
                  – except skipping dialogue by tapping and swiping to skip to next player-controllable state. */
                 break;
-            case GameState.levelComplete:
+            case GameStateScriptableObject.GameState.levelComplete:
                 /* Fade to black, load next scene, transition into level-start state.
                 (Possibly set state to be level-start before calling the load-next-scene function) */
                 break;
-            case GameState.optionsMenuOpened:
+            case GameStateScriptableObject.GameState.optionsMenuOpened:
                 //Should pause all game-logic and behaviours. 
                 Time.timeScale = 0;
 
@@ -44,7 +44,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public static GameState GetGameState()
+    public static GameStateScriptableObject.GameState GetGameState()
     {
         return gameState;
     }
