313de5cb5438b4a1728f68b61ac833e01a2eeff3
fredrig <neumannze@hotmail.com>
2019-11-04 14:29:27 +0100

    Created sound prefabs for music + scripts for music layers

---
+++ GraduationGame unity/Assets/Scripts/Sound Scripts/AddLayer5Music.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddLayer5Music : MonoBehaviour {
+
+private bool IsInCollider = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.RegisterGameObj(gameObject);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player" || IsInCollider) { return; }
+        IsInCollider = true;
+        AkSoundEngine.PostEvent("Set_Z1_Music_5", gameObject);
+        AkSoundEngine.PostEvent("Set_Z2_Music_5", gameObject);
+        AkSoundEngine.PostEvent("Set_Z3_Music_5", gameObject);
+        AkSoundEngine.PostEvent("Set_Z4_Music_5", gameObject);
+    }
+}
+    
---
+++ GraduationGame unity/Assets/Scripts/Sound Scripts/AddLayer6Music.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddLayer6Music : MonoBehaviour {
+
+private bool IsInCollider = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.RegisterGameObj(gameObject);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player" || IsInCollider) { return; }
+        IsInCollider = true;
+        AkSoundEngine.PostEvent("Set_Z1_Music_6", gameObject);
+        AkSoundEngine.PostEvent("Set_Z2_Music_6", gameObject);
+        AkSoundEngine.PostEvent("Set_Z3_Music_6", gameObject);
+        AkSoundEngine.PostEvent("Set_Z4_Music_6", gameObject);
+    }
+}
+    
