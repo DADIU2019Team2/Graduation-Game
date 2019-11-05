1ba98dbba7e3176f90127e78e473e7b155439671
LasseAafeldt <superlla@hotmail.com>
2019-11-05 13:15:04 +0100

    smoothCam update
    cam now changes direction as intended, also has smooth rotation

--- GraduationGame unity/Assets/Scripts/DemoStuff/SmoothPlayerCamFollow.cs
+++ GraduationGame unity/Assets/Scripts/DemoStuff/SmoothPlayerCamFollow.cs
@@ -11,6 +11,8 @@ public class SmoothPlayerCamFollow : MonoBehaviour
     [SerializeField] private KinematicCharacterMotor charMotor;
 
     [SerializeField] private float smoothTime = 0.1f;
+    [Tooltip("The speed at which the camera rotates with... around 1.5 seems to be good")]
+    [SerializeField] private float rotationSpeed = 1.5f;
 
     [SerializeField] private Vector3 localOffSet;
     private Vector3 worldOffset;
@@ -21,7 +23,6 @@ public class SmoothPlayerCamFollow : MonoBehaviour
     private void Start()
     {
         localPosInfrontOfPlayer = new Vector3(2, 0, 0); //update this dynamicly
-
     }
     private void LateUpdate()
     {
@@ -37,9 +38,22 @@ public class SmoothPlayerCamFollow : MonoBehaviour
 
     Vector3 updateLookAt()
     {
+
+        Vector3 desiredLookAtPoint = updateDesiredLookAtPointWorld();
+
+        Quaternion targetRotation = Quaternion.LookRotation(desiredLookAtPoint - transform.position, Vector3.up);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        
+
+        return desiredLookAtPoint;
+    }
+
+    Vector3 updateDesiredLookAtPointWorld()
+    {
         //do some dynamic localPos infront of player
         Vector3 charToFollowVelocity = charMotor.Velocity;
-        
+
         Vector3 dynamicLookAtPoint = new Vector3(charToFollowVelocity.x / 2, 0 + worldOffset.y, 0);
         //Debug.Log("Dynamic lookat point = " + dynamicLookAtPoint);
         float distToLookatPoint = Mathf.Clamp(dynamicLookAtPoint.magnitude, 0f, maxDistToLookAtPoint);
@@ -47,19 +61,9 @@ public class SmoothPlayerCamFollow : MonoBehaviour
         localPosInfrontOfPlayer = dynamicLookAtPoint.normalized * distToLookatPoint;
         //Debug.Log("local in-front of player = " + localPosInfrontOfPlayer);
 
-        Vector3 worldPosInFrontOfPlayer = Vector3.zero;
-        if (!CharacterMovement.GetIsFacingRight()) //therefore giong left (counterClockwise)
-        {
-            worldPosInFrontOfPlayer = playerToFollow.TransformPoint(-localPosInfrontOfPlayer.x, 
+        Vector3 desiredWorldPosInFrontOfPlayer = playerToFollow.TransformPoint(localPosInfrontOfPlayer.x,
                 localPosInfrontOfPlayer.y, localPosInfrontOfPlayer.z);
-        }
-        else if (CharacterMovement.GetIsFacingRight()) //therefore giong right (Clockwise)
-        {
-            worldPosInFrontOfPlayer = playerToFollow.TransformPoint(localPosInfrontOfPlayer.x, 
-                localPosInfrontOfPlayer.y, localPosInfrontOfPlayer.z);
-        }
-        transform.LookAt(worldPosInFrontOfPlayer, Vector3.up);
-        return worldPosInFrontOfPlayer;
+        return desiredWorldPosInFrontOfPlayer;
     }
 
     private void OnDrawGizmos()
