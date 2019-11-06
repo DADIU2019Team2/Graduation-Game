946dc1f2c3d714510cd2c621d02e6d1a9e42f966
Rutaceae <jamiemoeller@yahoo.dk>
2019-11-06 14:39:51 +0100

    Inputmanager changes

--- GraduationGame unity/Assets/Scripts/InputManager.cs
+++ GraduationGame unity/Assets/Scripts/InputManager.cs
@@ -26,16 +26,29 @@ public class InputManager : MonoBehaviour
 
     private void Start()
     {
-        //Debug.Assert(mirroredAngle(10) == 170, "10 mirrored = 170");
-        //Debug.Assert(mirroredAngle(190) == -10, "190 mirrored = -10");
+        Debug.Assert(mirroredAngle(10) == 170, "10 mirrored = 170");
+        Debug.Assert(mirroredAngle(190) == -10, "190 mirrored = -10");
+        Debug.Assert(mirroredAngle(200) == -20, "200 mirrored = -20");
+        Debug.Assert((-20 + 360) % 360 == 340, "-20%360 == 340");
+        Debug.Assert(340 % 360 == 340);
+
+
+        Debug.Assert(!(30 > (mirroredAngle(swipeAngleThresholds.swipeUpRightAngles0) + 360) % 360 && (mirroredAngle(swipeAngleThresholds.swipeUpRightAngles1) + 360) % 360 > 30),
+         "Testing with 30 deg swipe backwards");
+        Debug.Log((mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles0) + 360) % 360);
+        Debug.Log((mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles1) + 360) % 360);
+
+        Debug.Assert(Is_P3_between_P1_and_P2(swipeAngleThresholds.swipeUpRightAngles1, swipeAngleThresholds.swipeUpRightAngles0, 30), "swipeangletest with new function");
+
 
 
     }
 
     float mirroredAngle(float angle)
     {
-        return 180 + 360 - angle;
+        return 180 - angle;
     }
+
 
     void Update()
     {
@@ -58,8 +71,8 @@ public class InputManager : MonoBehaviour
                         mostRecentSwipeType = SwipeTypeOfAngle(swipeAngle, isFacingRight);
                         OnSwipeEvent.Raise();
                     }
-                    
-                    
+
+
 
                     break;
             }
@@ -91,20 +104,29 @@ public class InputManager : MonoBehaviour
         }
         else if (!isFacingRight)
         {
-            if (swipeAngle < mirroredAngle(swipeAngleThresholds.swipeUpRightAngles0) % 360 && swipeAngle > mirroredAngle(swipeAngleThresholds.swipeUpRightAngles1) % 360)
+            if (Is_P3_between_P1_and_P2(mirroredAngle(swipeAngleThresholds.swipeDownAngles1), mirroredAngle(swipeAngleThresholds.swipeDownAngles0), swipeAngle))
+            {
+
+                return SwipeType.swipeDown;
+            }
+            if (Is_P3_between_P1_and_P2(mirroredAngle(swipeAngleThresholds.swipeUpRightAngles1), mirroredAngle(swipeAngleThresholds.swipeUpRightAngles0), swipeAngle))
             {
                 return SwipeType.swipeForwardUp;
             }
-            if (swipeAngle < mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles0) % 360 && swipeAngle > mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles1) % 360)
+            if (Is_P3_between_P1_and_P2(mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles1), mirroredAngle(swipeAngleThresholds.swipeBackwardsAngles0), swipeAngle))
             {
                 return SwipeType.swipeBackwards;
-            }
-            if (swipeAngle < mirroredAngle(swipeAngleThresholds.swipeDownAngles0) % 360 && swipeAngle > mirroredAngle(swipeAngleThresholds.swipeDownAngles1) % 360)
-            {
-                return SwipeType.swipeDown;
             }
         }
         return SwipeType.none; //Stuff bugged out or the inputsettings on SwipeAngleThreshholds
     }
 
+    bool Is_P3_between_P1_and_P2(double p1, double p2, double p3)
+    {
+        double p1_p2, p1_p3;
+        p1_p2 = (p2 - p1 + 360)%360;
+        p1_p3 = (p3 - p1 + 360)%360;
+
+        return (p1_p2 <= 180) != (p1_p3 > p1_p2);
+    }
 }
