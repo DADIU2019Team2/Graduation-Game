5d0e6a5f71ee13f663504cf04f756cd6b4107e1b
LasseAafeldt <superlla@hotmail.com>
2019-11-13 10:52:55 +0100

    made checkpoint system that also resets char on reset level

---
+++ GraduationGame unity/Assets/Scripts/ScriptableObjectScripts/SaveTransform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Save Tranform", menuName = "ScriptableObject/Save Transform")]
+public class SaveTransform : ScriptableObject
+{
+    //[SerializeField] private Transform transformToSave;
+    [SerializeField] private Vector3 posToSave;
+
+    /*public void SetTransformToSave(Transform transform)
+    {
+        transformToSave = transform;
+    }
+
+    public Transform GetTransformToSave()
+    {
+        return transformToSave;
+    }*/
+
+    public void SetPosToSave(Transform trans)
+    {
+        posToSave = trans.position;
+    }
+    public Vector3 GetPosToSave()
+    {
+        return posToSave;
+    }
+}
