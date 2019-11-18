309512e83658cdf3ea680099c677274958d246e6
AndersHansen93 <31648459+AndersHansen93@users.noreply.github.com>
2019-11-18 12:54:26 +0100

    unlock skins on end level, continue button working.

---
+++ GraduationGame unity/Assets/Scripts/SaveLoad/UnlockItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnlockItem
+{
+
+    public static void UnlockItems(Unlockables item)
+    {
+        GameSave.currentSave.UpdateUnlocks(item,true);
+    }
+
+}
