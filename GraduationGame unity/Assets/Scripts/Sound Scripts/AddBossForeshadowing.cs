0e68db8e79fcd4ef30edc127fbdb00405095b699
fredrig <neumannze@hotmail.com>
2019-11-19 15:46:17 +0100

    Added sound script and prefab for boss foreshadowing

---
+++ GraduationGame unity/Assets/Scripts/Sound Scripts/AddBossForeshadowing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBossForeshadowing : MonoBehaviour
+{
+
+private bool IsInCollider = false;
+    void Start()
+    {
+        AkSoundEngine.RegisterGameObj(gameObject);
+    }
+
+private void OnTriggerEnter (Collider other)
+{
+    if (other.tag != "Player" || IsInCollider) { return; }
+    AkSoundEngine.SetRTPCValue("Boss_Foreshadowing", 1); 
+}
+
+}
