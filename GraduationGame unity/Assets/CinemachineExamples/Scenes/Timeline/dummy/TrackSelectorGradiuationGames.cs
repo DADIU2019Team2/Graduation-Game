3352fde6374c3f7c95e8eabf217e0f356cc5c949
AndersHansen93 <31648459+AndersHansen93@users.noreply.github.com>
2019-11-20 09:42:04 +0100

    reverse cam option

---
+++ GraduationGame unity/Assets/CinemachineExamples/Scenes/Timeline/dummy/TrackSelectorGradiuationGames.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+
+public class TrackSelectorGradiuationGames : MonoBehaviour
+{
+
+
+    public void TransitionToCam(GameObject cam)
+    {
+        cam.GetComponent<CinemachineDollyCart>().enabled = true;
+        cam.GetComponent<PlayableDirector>().enabled = true;
+        cam.GetComponent<CinemachineDollyCart>().m_Speed = Mathf.Abs(cam.GetComponent<CinemachineDollyCart>().m_Speed);
+    }
+
+    public void ResetCam(GameObject cam)
+    {
+        cam.GetComponent<CinemachineDollyCart>().m_Position = 0;
+        cam.GetComponent<CinemachineDollyCart>().enabled = false;
+        cam.GetComponent<PlayableDirector>().initialTime = 0;
+        cam.GetComponent<PlayableDirector>().enabled = false;
+
+    }
+
+    public void ReverseCam(GameObject cam)
+    {
+        Debug.Log("");
+        cam.GetComponent<CinemachineDollyCart>().enabled = true;
+        cam.GetComponent<CinemachineDollyCart>().m_Speed = Mathf.Abs(cam.GetComponent<CinemachineDollyCart>().m_Speed) * -1;
+        cam.GetComponent<PlayableDirector>().enabled = true;
+    }
+}
