0432d430b1298f0443e82cf383e4ff80d89ed5a0
LasseAafeldt <superlla@hotmail.com>
2019-11-28 15:05:27 +0100

    cam with deadzone (needs y smoothing)

--- GraduationGame unity/Assets/Scripts/DemoStuff/SmoothPlayerCamFollow.cs
+++ GraduationGame unity/Assets/Scripts/DemoStuff/SmoothPlayerCamFollow.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 using KinematicCharacterController;
 using KinematicTest.controller;
 
+
 public class SmoothPlayerCamFollow : MonoBehaviour
 {
-    [SerializeField] private Transform playerToFollow;
+    private enum ZoeMoveDir { idle, up, down, left, right}
+    private ZoeMoveDir currentMoveDir;
 
-    //character controller for ref to movespeed/direction???
-    //[SerializeField] private CharacterMovement moveScript; 
+    [Header("Other component references")]
+    [SerializeField] private Transform playerToFollow;
     [SerializeField] private KinematicCharacterMotor charMotor;
 
-    [SerializeField] private float smoothTime = 0.1f;
-
+    [Header("Camera properties")]
     [Tooltip("The speed at which the camera rotates with... around 1.5 seems to be good")] [SerializeField]
     private float rotationSpeed = 1.5f;
+    [SerializeField] private float smoothTime = 0.1f;
 
     [SerializeField] private Vector3 localOffSet;
-    private Vector3 worldOffset;
     [SerializeField] private float maxDistToLookAtPoint = 5f;
-    private Vector3 camVelocity;
-    private Vector3 localPosInfrontOfPlayer;
-    private float zoomOutTimer;
-    private float zoomOutLerp;
     [SerializeField] private float stopZoomOutDistance;
     public float zoomOutAfterSeconds;
     public float zoomOutDuration;
+
+    [Header("Deadzone Definition")]
+    [SerializeField] private float deadZoneXLeft;
+    [SerializeField] private float deadZoneXRight;
+    [SerializeField] private float deadZoneYTop = 3f;
+    [SerializeField] private float deadZoneYBot = 2.5f;
+
+    private float xDiff;
+    private float yDiff;
+
+    private Vector3 newdesiredPos;
+
+    private Vector3 worldOffset;
+    private Vector3 localPosInfrontOfPlayer;
+    private Vector3 camVelocity;
+    private float zoomOutTimer;
+    private float zoomOutLerp;
 
     private void Start()
     {
         if (Mathf.Approximately(zoomOutDuration,0f))
             zoomOutDuration = 0.1f;
         localPosInfrontOfPlayer = new Vector3(2, 0, 0); //update this dynamicly
+        currentMoveDir = ZoeMoveDir.idle;
     }
 
     private void LateUpdate()
@@ -63,8 +78,99 @@ public class SmoothPlayerCamFollow : MonoBehaviour
             worldOffset.z = localOffSet.z - actualZoomOut;
         }
 
-        transform.position = Vector3.SmoothDamp(transform.position, playerToFollow.position + worldOffset,
+        Vector3 tempDesiredPos = DesiredCamPos();
+        float distToDesiredPos = (tempDesiredPos - transform.position).magnitude;
+
+        //if(distToDesiredPos > 0.1)
+        //{
+        //}
+        
+        //Do seperate smoothing speperate x and y
+
+        transform.position = Vector3.SmoothDamp(transform.position, tempDesiredPos,
             ref camVelocity, smoothTime);
+    }
+
+    Vector3 DesiredCamPos()
+    {
+        bool wasChanged = false;
+        Vector3 oldDesiredPos = new Vector3();
+        oldDesiredPos = transform.position;
+        newdesiredPos = oldDesiredPos;
+
+        xDiff = (playerToFollow.position.x + localOffSet.x) - (transform.position.x + localOffSet.x);
+        yDiff = (playerToFollow.position.y + localOffSet.y) - (transform.position.y + localOffSet.y);
+
+        if (xDiff > deadZoneXRight || CheckMoveDir(currentMoveDir))
+        {
+            newdesiredPos.x = playerToFollow.position.x + localOffSet.x;
+            wasChanged = true;
+            currentMoveDir = ZoeMoveDir.right;
+        }
+        if (xDiff < -deadZoneXLeft || CheckMoveDir(currentMoveDir))
+        {
+            newdesiredPos.x = playerToFollow.position.x + localOffSet.x;
+            wasChanged = true;
+            currentMoveDir = ZoeMoveDir.left;
+        }
+
+        if (charMotor.GroundingStatus.FoundAnyGround)
+        {
+            newdesiredPos.y = playerToFollow.position.y + localOffSet.y;
+            wasChanged = true;
+        }
+        if (yDiff > deadZoneYTop || CheckMoveDir(currentMoveDir))
+        {
+            newdesiredPos.y = playerToFollow.position.y + localOffSet.y;
+            wasChanged = true;
+            currentMoveDir = ZoeMoveDir.up;
+        }
+
+        if(yDiff < -deadZoneYBot || CheckMoveDir(currentMoveDir))
+        {
+            newdesiredPos.y = playerToFollow.position.y + localOffSet.y;
+            wasChanged = true;
+            currentMoveDir = ZoeMoveDir.down;
+        }
+
+        newdesiredPos.z = playerToFollow.position.z + localOffSet.z;
+
+        if (wasChanged)
+            return newdesiredPos;
+        else
+            return oldDesiredPos;
+    }
+
+    bool CheckMoveDir(ZoeMoveDir dirToCheck)
+    {
+        switch (dirToCheck)
+        {
+            case ZoeMoveDir.up:
+                if(charMotor.Velocity.y >= 0)
+                {
+                    return true;
+                }
+                break;
+            case ZoeMoveDir.down:
+                if(charMotor.Velocity.y <= 0)
+                {
+                    return true;
+                }
+                break;
+            case ZoeMoveDir.left:
+                if(charMotor.Velocity.x <= 0)
+                {
+                    return true;
+                }
+                break;
+            case ZoeMoveDir.right:
+                if(charMotor.Velocity.x >= 0)
+                {
+                    return true;
+                }
+                break;
+        }
+        return false;
     }
 
     Vector3 updateLookAt()
@@ -106,5 +212,45 @@ public class SmoothPlayerCamFollow : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawLine(transform.position, updateLookAt());
         }
+        DrawCamDeadZone();
+    }
+
+
+    void DrawCamDeadZone()
+    {
+        bool drawLeft = true;
+        bool drawRight = true;
+        float tempDeadXLeft = deadZoneXLeft;
+        float tempDeadXRight = deadZoneXRight;
+        if (deadZoneXLeft == 0)
+        {
+            tempDeadXLeft = 5f;
+            drawLeft = false;
+        }
+        if(deadZoneXRight == 0)
+        {
+            tempDeadXRight = 5f;
+            drawRight = false;
+        }
+
+        Vector3 topLeftCorner = new Vector3(transform.position.x - tempDeadXLeft, transform.position.y + deadZoneYTop,
+            playerToFollow.position.z -2);
+
+        Vector3 topRightCorner = new Vector3(transform.position.x + tempDeadXRight, transform.position.y + deadZoneYTop,
+            playerToFollow.position.z -2);
+
+        Vector3 botLeftCorner = new Vector3(transform.position.x - tempDeadXLeft, transform.position.y - deadZoneYBot,
+            playerToFollow.position.z -2);
+
+        Vector3 botRightCorner = new Vector3(transform.position.x + tempDeadXRight, transform.position.y - deadZoneYBot,
+            playerToFollow.position.z -2);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(topLeftCorner, topRightCorner);
+        if(drawLeft)
+            Gizmos.DrawLine(topLeftCorner, botLeftCorner);
+        Gizmos.DrawLine(botLeftCorner, botRightCorner);
+        if(drawRight)
+            Gizmos.DrawLine(botRightCorner, topRightCorner);
     }
 }
