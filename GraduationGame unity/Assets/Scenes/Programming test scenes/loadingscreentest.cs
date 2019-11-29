142cfab1c7d5c645bd66ecdfd33915858aec206b
atange-filmskolen <52198175+atange-filmskolen@users.noreply.github.com>
2019-11-29 13:27:39 +0100

    Update loadingscreentest.cs

--- GraduationGame unity/Assets/Scenes/Programming test scenes/loadingscreentest.cs
+++ GraduationGame unity/Assets/Scenes/Programming test scenes/loadingscreentest.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class loadingscreentest : MonoBehaviour
 {
