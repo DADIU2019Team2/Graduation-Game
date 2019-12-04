be1b6e9c9d6695bef636942e409387323b387a86
AndersHansen93 <31648459+AndersHansen93@users.noreply.github.com>
2019-12-04 11:50:03 +0100

    credits transition to main menu after its done

---
+++ GraduationGame unity/Assets/Scripts/CreditsEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsEnd : MonoBehaviour
+{
+    public void CreditsDone()
+    {
+        Debug.Log("ANIM EVENT");
+        //GameManager.RequestGameStateChange(GameStateScriptableObject.GameState.levelComplete);
+        AkSoundEngine.StopAll();
+        int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+        Debug.Log("Amount of scenes vs the index we want to load:" + SceneManager.sceneCountInBuildSettings + " vs " + nextScene);
+        if (SceneManager.sceneCountInBuildSettings - 1 < nextScene)
+        {
+            nextScene = 0;
+            //Should have this be the credits scene instead.
+        }
+        SceneManager.LoadScene(nextScene);
+    }
+}
