77d99052499c66e8e0edf9a183ab9749ea6523ce
fredrig <neumannze@hotmail.com>
2019-12-04 10:50:11 +0100

    Sound script changes

--- GraduationGame unity/Assets/Scripts/Sound Scripts/MusicZone2NoFadeIn.cs
+++ GraduationGame unity/Assets/Scripts/Sound Scripts/MusicZone2NoFadeIn.cs
@@ -21,6 +21,20 @@ public class MusicZone2NoFadeIn : MonoBehaviour
         AkSoundEngine.PostEvent("Play_Zone2_Music", gameObject);
         
         AkSoundEngine.SetRTPCValue("Happy_Drumz", 2);
+        
+        AkSoundEngine.SetRTPCValue("Happy_Bass", 1);
+        
+        AkSoundEngine.SetRTPCValue("Happy_Synth", 1);
+
+        AkSoundEngine.SetRTPCValue("Music_Layer_Z1", 1);
+
+        AkSoundEngine.SetRTPCValue("Music_Layer_Z2", 1);
+
+        AkSoundEngine.SetRTPCValue("Music_Layer_Z3", 1);
+        
+        AkSoundEngine.SetRTPCValue("Music_Layer_Z4", 1);
+
+        AkSoundEngine.SetRTPCValue("Angry_Music_Layer", 0);
 
         AkSoundEngine.PostEvent("Play_Ambience_Z2", gameObject);
     }
